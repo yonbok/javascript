62e6fa03d7ae27354d369fda602671cfd3b63038
yonbok <na.re.zutomayo14@gmail.com>
2023-09-05 14:19:10 +0000

    変数宣言の再代入まで完了

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-let max = 100;
-let num = 1;
-let count = 0;
+let i;
+let num = 0;
 
-while(num < max){
-  num = num * 2;
-  count = count + 1;
+for(i = 1; i < 11; i++){ 　//i++はi=i+1と同じ
+  num = num + i;
 }
+alert("1から10まで足し算した結果は" + num + "です");
 
-alert("2をかけ続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert("2をかけ続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
 
 // let orange = 200;
 // let apple = 120;
---
+++ sample.js
@@ -0,0 +1,41 @@
+// constによる再代入
+const nickname = "taro"
+console.log(nickname)
+nickname = "jiro"
+console.log(nickname)
+
+
+// letによる再代入
+// let nickname ="taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+
+// varによる再代入
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+
+
+// constによる再宣言
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+
+
+// letによる再宣言
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+
+// varによる再宣言
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
