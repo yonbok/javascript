50c68dfbef8d0cf80451f5c17afb9d0a4fac2f2d
yonbok <na.re.zutomayo14@gmail.com>
2023-09-05 14:42:05 +0000

    変数宣言のスコープまで完了

--- sample.js
+++ sample.js
@@ -1,8 +1,41 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+console.log(i)
+
+
+// function foo() {
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+
+// var str = "webcamp"  //グローバルスコープ
+
+// function foo() {
+//   console.log(str)
+//   var y = "hello"  //関数スコープ
+// }
+
+// foo()
+// console.log(y)
+
+
+
+
+
 // constによる再代入
-const nickname = "taro"
-console.log(nickname)
-nickname = "jiro"
-console.log(nickname)
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
 
 
 // letによる再代入
