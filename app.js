7691d5ec09b421262e7fbd560865b9d2be7641d8
yonbok <na.re.zutomayo14@gmail.com>
2023-09-09 14:54:34 +0000

    乱数を生成する関数の設定まで完了

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-let promptStr = prompt("何か好きな文字を入力してください。");
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
 
-alert(promptStr);
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  } else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  } else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+
+  return js_hand;
+}
+
+
+// let user_hand = prompt("じゃんけんの手をグー、チョキ、パーから選んでください。");
+// alert("あなたの選んだ手は" + user_hand + "です。");
+
+
+
+// let promptStr = prompt("何か好きな文字を入力してください。");
+
+// alert(promptStr);
 
 
 
