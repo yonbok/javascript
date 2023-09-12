2be934775842065a073498e9161c7cb5b12055a9
yonbok <na.re.zutomayo14@gmail.com>
2023-09-12 02:23:06 +0000

    関数の章完了

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ function getJShand(){
   let hand_name;
 
   if(js_hand_num == 0){
-    js_hand = "グー";
+    hand_name = "グー";
   } else if(js_hand_num == 1){
-    js_hand = "チョキ";
+    hand_name = "チョキ";
   } else if(js_hand_num == 2){
-    js_hand = "パー";
+    hand_name = "パー";
   }
 
-  return js_hand;
+  return hand_name;
 }
 
 // ユーザーの手とJavaScriptのじゃんけんの手を比べる関数
@@ -31,8 +31,32 @@ function winLose(user, js){
   let winLoseStr;
 
   if(user == "グー"){
-
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "勝ち"){
+      winLoseStr = "パー";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
   }
+
+  return winLoseStr;
 }
 
 
